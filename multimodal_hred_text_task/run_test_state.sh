3c9f3ac67a3f521cde2515918462b601b6fc1b83
amritasaha1812 <amrita.saha87@gmail.com>
2018-03-30 13:42:01 -0400

    made mior changes to run_test_state.sh

--- multimodal_hred_text_task/run_test_state.sh
+++ multimodal_hred_text_task/run_test_state.sh
@@ -1,3 +1,7 @@
+########################################
+# EXAMPLE RUN: ./run_test_state.sh <DATA_DIR> <DUMP_DIR> 1 <TEST_STATE> (for V1 dataset) and ./run.sh <DATA_DIR> <DUMP_DIR> 2 <TEST_STATE>(for V2 dataset)
+# TEST_STATE can be either of the following: ask_attribute,buy,celebrity,do_not_like_earlier_show_result,do_not_like_n_show_result,do_not_like_show_result,filter_results,go_with,like_earlier_show_result,like_n_show_result,like_show_result,show_orientation,show_result,show_similar_to,sort_results,suited_for
+########################################
 cp params_test_v$3.py params_test.py
 mkdir $2/e_test_state
 mkdir $2/o_test_state
